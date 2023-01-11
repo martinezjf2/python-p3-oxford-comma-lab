69887b02f866b45e256e420c4d0f14cbee383881
Jeffrey <martinezjf2@gmail.com>
2023-01-11 00:04:06 -0500

    Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,27 @@
 def oxford_comma(items):
-    return None
+   
+    # if len(items) == 1:
+    #     return "".join(items)
+    # elif len(items) == 2:
+    #   return  " and ".join(items)
+    # elif len(items) == 3:
+    #     sen = []
+    #     for i, val in items:
+    #         while i != 2:
+    #             sentence = f"{val}, "
+    #             sen.append(sentence)
+    #         sent = f"and {val}"
+    #         sen.append(sent)
+    #     return sen  
+    
+    if len(items) == 1:
+        return str(items[0])
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    return "{}, and {}".format(", ".join(items[:-1]), items[-1])  
+            
+
+# Resource used: https://stackoverflow.com/questions/19838976/grammatical-list-join-in-python
+        
+
+    
